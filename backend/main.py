0b00d03b38128c2b9009e2e3694840cde006761d
DivyanshDeshmukh <omdeshmukh456@gmail.com>
2026-01-15 00:30:14 +0530

    added dummy function for like

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Header, HTTPException
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
@@ -14,6 +14,16 @@ supabase: Client = create_client(
     SUPABASE_SERVICE_KEY
 )
 
+def get_user_id(authorization: str = Header(...)):
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    token = authorization.split(" ")[1]
+
+    # For now we just return the token
+    # Later we will extract user_id from JWT
+    return token
+
 
 app = FastAPI()
 
@@ -26,3 +36,7 @@ def health():
 def db_test():
     res = supabase.table("users").select("id").limit(1).execute()
     return {"data": res.data}
+
+@app.post("/like/{to_user_id}")
+def like_user(to_user_id: str, token=Depends(get_user_id)):
+    pass
