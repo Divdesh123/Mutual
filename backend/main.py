26b3414d888370641f8cc44396cfe5e69a1a54bf
DivyanshDeshmukh <omdeshmukh456@gmail.com>
2026-01-14 23:51:45 +0530

    connected to supabase

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+
+
+
+supabase: Client = create_client(
+    SUPABASE_URL,
+    SUPABASE_SERVICE_KEY
+)
+
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/db-test")
+def db_test():
+    res = supabase.table("users").select("id").limit(1).execute()
+    return {"data": res.data}
